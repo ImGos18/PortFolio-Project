a4949ba3d314e3d136121bb7a8d02e89ba240f87
ImGos18 <gustavoesoler0@gmail.com>
2025-10-08 14:04:48 -0300

    optimizaciones finales, fix a la barra de scroll y compresion de imagenes

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -6,8 +6,37 @@ const cerrarNav = function () {
 };
 
 document.querySelectorAll(".offcanvas-end li").forEach((link) => {
-  link.addEventListener("click", cerrarNav);
+  link.addEventListener("click", function () {
+    cerrarNav();
+  });
 });
 document
   .getElementById("offcanvasDarkNavbarLabel")
   .addEventListener("click", cerrarNav);
+
+const listaLinks = document.querySelector("#linksNav");
+const listaItem = listaLinks.querySelectorAll("li");
+
+listaItem.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const enlaceSeccion = link.querySelector("a");
+    const referencia = enlaceSeccion.getAttribute("href");
+    const seccion = document.querySelector(referencia);
+    const posicion = seccion.getBoundingClientRect();
+    const PrincipioSeccion = window.scrollY + posicion.top;
+    const scrollOffset = function (offset) {
+      window.scrollTo({
+        top: PrincipioSeccion - offset,
+        behavior: "smooth",
+      });
+    };
+    console.log(PrincipioSeccion);
+    if (window.innerWidth <= 990) {
+      scrollOffset(170);
+    } else {
+      scrollOffset(200);
+    }
+    console.log(window.innerWidth);
+  });
+});
